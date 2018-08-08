cd46b4ff59189ed40be8e4affaa4e05a56b88faa
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-08 18:38:00 +0100

    More succint

--- src/hydrateClient.jsx
+++ src/hydrateClient.jsx
@@ -4,13 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 const hydrateClient = routes => {
-  const data = JSON.parse(
-    window.document.getElementById('uni-data').textContent,
+  const { textContent } = JSON.parse(
+    window.document.getElementById('uni-data'),
   );
 
   hydrate(
     <BrowserRouter>
-      <App routes={routes} initialData={data} />
+      <App routes={routes} initialData={textContent} />
     </BrowserRouter>,
     document.getElementById('__uni__'),
   );
