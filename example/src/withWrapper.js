65a69fb5f060c6c66ef6b22d7b2dd817cc4e9491
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-11 16:22:29 +0100

    Add example with apollo

--- example/src/withWrapper.js
+++ example/src/withWrapper.js
@@ -1,13 +1,35 @@
+import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ServerData } from '@jtart/uni';
 
 function withWrapper(App) {
   this.sheet = new ServerStyleSheet();
-  return this.sheet.collectStyles(App);
+
+  this.client = new ApolloClient({
+    ssrMode: true,
+    link: createHttpLink({
+      uri: 'https://fakerql.com/graphql',
+    }),
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      {this.sheet.collectStyles(App)}
+    </ApolloProvider>
+  );
 }
 
 withWrapper.getTags = function() {
   const styles = this.sheet.getStyleElement();
-  return [styles];
+
+  const initialApolloData = this.client.extract();
+
+  return [styles, <ServerData id="__APOLLO_DATA__" data={initialApolloData} />];
 };
 
 export default withWrapper;
