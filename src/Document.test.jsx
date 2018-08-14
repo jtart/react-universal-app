cbab59a0cc833f661d0d09d32f379e3d90925ba7
Jordan Tart <jordan.tart3@gmail.com>
2018-08-14 01:02:54 +0100

    Test document

---
+++ src/Document.test.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Document from './Document';
+import Helmet from 'react-helmet';
+
+Helmet.renderStatic = jest.fn().mockReturnValue({
+  htmlAttributes: {
+    toComponent: jest.fn().mockReturnValue({ lang: 'en_GB' }),
+  },
+  title: {
+    toComponent: jest.fn().mockReturnValue(<title />),
+  },
+  meta: {
+    toComponent: jest.fn().mockReturnValue(<meta />),
+  },
+  link: {
+    toComponent: jest
+      .fn()
+      .mockReturnValue([<link key="l-1" />, <link key="l-2" />]),
+  },
+  bodyAttributes: {
+    toComponent: jest.fn().mockReturnValue({ dir: 'ltr' }),
+  },
+});
+
+describe('Document', () => {
+  it('should render correctly', () => {
+    const doc = (
+      <Document
+        additional={[
+          <style key="s-1" data-test-styles-one />,
+          <style key="s-2" data-test-styles-two />,
+        ]}
+        html={<h1>This is some html</h1>}
+        data={{ data: 'This is some data!' }}
+        scripts={['https://example.com/one.js', 'https://example.com/two.js']}
+      />
+    );
+    const wrapper = mount(doc);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
--- test/jestSetup.js
+++ test/jestSetup.js
@@ -1,6 +1,7 @@
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 global.shallow = shallow;
+global.mount = mount;
