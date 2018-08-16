db396d80ccf21168538ca1de2ba7cfd296492e15
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-16 23:58:30 +0100

    Simplify App tests

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { withRouter } from 'react-router-dom';
 import getRouteAndMatch from './getRouteAndMatch.js';
 import loadInitialProps from './loadInitialProps.js';
 
-class App extends Component {
+export class App extends Component {
   constructor(props) {
     super(props);
 
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import App from './App';
+import { App } from './App';
 import * as reactRouterConfig from 'react-router-config';
-import { BrowserRouter } from 'react-router-dom';
-
-reactRouterConfig.renderRoutes = jest
-  .fn()
-  .mockReturnValue(<h1>A rendered route!</h1>);
 
 describe('App', () => {
-  it('should return rendered routes', () => {
-    const wrapper = mount(
-      <BrowserRouter>
-        <App routes={[]} initialData={{ data: 'Some of it' }} />
-      </BrowserRouter>,
-    );
+  it('should return rendered routes with initial data', () => {
+    const data = 'Some initial data';
+
+    reactRouterConfig.renderRoutes = jest.fn().mockReturnValue(<h1>{data}</h1>);
+
+    const wrapper = shallow(<App routes={[]} initialData={{ data }} />);
 
     expect(reactRouterConfig.renderRoutes).toHaveBeenCalledTimes(1);
     expect(reactRouterConfig.renderRoutes).toHaveBeenCalledWith([], {
-      data: 'Some of it',
+      data,
     });
     expect(wrapper).toMatchSnapshot();
   });
