64bb1ac6bd62ac3a4b06de610eb56b7a55dbeca7
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-17 01:29:16 +0100

    Add tests for fetching props, rejecting fetch of props

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,59 +4,115 @@ import * as reactRouterConfig from 'react-router-config';
 import * as loadInitialProps from './loadInitialProps';
 import * as getRouteAndMatch from './getRouteAndMatch.js';
 
-loadInitialProps.default = jest.fn();
-getRouteAndMatch.default = jest.fn();
-
 describe('App', () => {
+  let wrapper;
+  let setStateSpy;
+  let loadInitialPropsSpy;
+  let getRouteAndMatchSpy;
+  const initialData = { data: 'Some initial data' };
+
+  reactRouterConfig.renderRoutes = jest
+    .fn()
+    .mockReturnValue(<h1>{initialData.data}</h1>);
+
+  beforeAll(() => {
+    wrapper = shallow(
+      <App
+        location={{ pathname: 'pathnameOne' }}
+        routes={[]}
+        initialData={initialData}
+      />,
+    );
+    setStateSpy = jest.spyOn(wrapper.instance(), 'setState');
+    loadInitialPropsSpy = spyOn(loadInitialProps, 'default');
+    getRouteAndMatchSpy = spyOn(getRouteAndMatch, 'default');
+  });
+
   it('should return rendered routes with initial data', () => {
-    const data = 'Some initial data';
-
-    reactRouterConfig.renderRoutes = jest.fn().mockReturnValue(<h1>{data}</h1>);
-
-    const wrapper = shallow(<App routes={[]} initialData={{ data }} />);
-
+    expect.assertions(5);
+    expect(getRouteAndMatchSpy).not.toHaveBeenCalled();
+    expect(loadInitialPropsSpy).not.toHaveBeenCalled();
     expect(reactRouterConfig.renderRoutes).toHaveBeenCalledTimes(1);
-    expect(reactRouterConfig.renderRoutes).toHaveBeenCalledWith([], {
-      data,
-    });
+    expect(reactRouterConfig.renderRoutes).toHaveBeenCalledWith(
+      [],
+      initialData,
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   describe('componentDidUpdate', () => {
-    let wrapper;
-    let setStateSpy;
-    beforeAll(() => {
-      wrapper = shallow(
-        <App
-          location={{ pathname: 'pathnameOne' }}
-          routes={[]}
-          initialData={{ data: 'Some of it' }}
-        />,
-      );
-      setStateSpy = jest.spyOn(wrapper.instance(), 'setState');
-    });
-
     describe('same location', () => {
       it('should not call set state with new data', () => {
         wrapper.setProps({ location: { pathname: 'pathnameOne' } });
 
         expect.assertions(3);
-        expect(getRouteAndMatch.default).not.toHaveBeenCalled();
-        expect(loadInitialProps.default).not.toHaveBeenCalled();
+        expect(getRouteAndMatchSpy).not.toHaveBeenCalled();
+        expect(loadInitialPropsSpy).not.toHaveBeenCalled();
         expect(setStateSpy).not.toHaveBeenCalled();
       });
     });
 
     describe('different location', () => {
       describe('no route or match', () => {
-        it('should not call set state with new data', () => {
+        it('should only call getRouteAndMatch', () => {
           const pathname = 'pathnameTwo';
+
+          getRouteAndMatch.default = jest.fn().mockReturnValue({});
+
           wrapper.setProps({ location: { pathname } });
 
           expect.assertions(3);
           expect(getRouteAndMatch.default).toHaveBeenCalledWith(pathname, []);
-          expect(loadInitialProps.default).not.toHaveBeenCalled();
+          expect(loadInitialPropsSpy).not.toHaveBeenCalled();
           expect(setStateSpy).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('rejected loadInitialProps', () => {
+        it('should console log the error', async () => {
+          const error = 'Error!';
+          spyOn(global.console, 'log');
+
+          getRouteAndMatch.default = jest
+            .fn()
+            .mockReturnValue({ route: 'route', match: 'match' });
+          loadInitialProps.default = jest
+            .fn()
+            .mockImplementation(() => Promise.reject(error));
+
+          wrapper.setProps({ location: { pathname: 'pathnameThree' } });
+
+          await loadInitialProps.default;
+
+          expect.assertions(2);
+          expect(global.console.log).toBeCalledWith(error);
+          expect(setStateSpy).not.toBeCalled();
+        });
+      });
+
+      describe('successful fetch of route, match, and initial props', () => {
+        it('should call set state with new data', async () => {
+          const pathname = 'pathnameFour';
+          const route = 'some route';
+          const match = 'a match';
+          const data = 'Really cool data';
+
+          getRouteAndMatch.default = jest
+            .fn()
+            .mockReturnValue({ route, match });
+          loadInitialProps.default = jest
+            .fn()
+            .mockImplementation(async () => data);
+
+          wrapper.setProps({ location: { pathname } });
+
+          await loadInitialProps.default;
+
+          expect.assertions(2);
+          expect(loadInitialProps.default).toHaveBeenCalledWith(route, {
+            match,
+          });
+          expect(setStateSpy).toBeCalledWith({ data });
         });
       });
     });
