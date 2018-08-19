a7167742fd0fd71e2ec28b4f6b8badf3a8ba3297
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-19 23:56:35 +0100

    Safely handle async getInitialProps

--- src/loadInitialProps.js
+++ src/loadInitialProps.js
@@ -1,4 +1,13 @@
-const loadInitialProps = async (route, ctx) =>
-  route.getInitialProps ? await route.getInitialProps(ctx) : {};
+const loadInitialProps = async (route, ctx) => {
+  if (!route.getInitialProps) {
+    return {};
+  }
+
+  try {
+    return await route.getInitialProps(ctx);
+  } catch (error) {
+    return error;
+  }
+};
 
 export default loadInitialProps;
