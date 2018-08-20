ff75857d6d3782715a64002f9cf3dcacee67ca42
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-20 20:57:26 +0100

    Use Link from react-router-dom

--- integration/src/app/components/Index.jsx
+++ integration/src/app/components/Index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Link } from '@jtart/uni';
+import { Link } from 'react-router-dom';
 
 const Index = ({ title, link }) => (
   <div>
