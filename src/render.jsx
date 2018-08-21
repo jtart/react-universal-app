fbdc027f010d6276a520e8c73a88939139c120e3
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-21 23:22:20 +0100

    Extract app rendering

--- src/render.jsx
+++ src/render.jsx
@@ -1,35 +1,9 @@
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
-
 import getRouteAndMatch from './getRouteAndMatch.js';
 import loadInitialProps from './loadInitialProps.js';
-
-import App from './App.jsx';
+import renderApp from './renderApp.jsx';
 import createDocument from './createDocument.js';
 
-async function renderApp(url, routes, data, withWrapper) {
-  const appHTML = renderToString(
-    await withWrapper.call(
-      this,
-      <StaticRouter location={url} context={{}}>
-        <App initialData={data} routes={routes} />
-      </StaticRouter>,
-    ),
-  );
-
-  const additionalHeadElements = [];
-
-  if (Object.hasOwnProperty.call(withWrapper, 'getAdditionalHeadElements')) {
-    const wrapperElements = withWrapper.getAdditionalHeadElements.call(this);
-
-    additionalHeadElements.push(...wrapperElements);
-  }
-
-  return { appHTML, additionalHeadElements };
-}
-
-async function render(url, routes, scripts, withWrapper = async App => App) {
+export default async function(url, routes, scripts, withWrapper = null) {
   const { route, match } = getRouteAndMatch(url, routes);
 
   if (!route) {
@@ -49,5 +23,3 @@ async function render(url, routes, scripts, withWrapper = async App => App) {
 
   return { statusCode: 200, html };
 }
-
-export default render;
