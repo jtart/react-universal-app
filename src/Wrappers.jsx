20ba9d408998a75e5431ed5e322f208f470c02b9
Jordan Tart <jordan.tart@bbc.co.uk>
2018-09-23 13:33:27 +0100

    Pass react-router router props

--- src/Wrappers.jsx
+++ src/Wrappers.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { StaticRouter, BrowserRouter } from 'react-router-dom';
 import Uni from './Uni.jsx';
 
-export const ClientUni = ({ data, routes }) => (
-  <BrowserRouter>
+export const ClientUni = ({ data, routes, ...routerProps }) => (
+  <BrowserRouter {...routerProps}>
     <Uni initialData={data} routes={routes} />
   </BrowserRouter>
 );
 
-export const ServerUni = ({ url, routes, data, routerContext = {} }) => (
-  <StaticRouter location={url} context={routerContext}>
+export const ServerUni = ({ data, routes, ...routerProps }) => (
+  <StaticRouter {...routerProps}>
     <Uni initialData={data} routes={routes} />
   </StaticRouter>
 );
